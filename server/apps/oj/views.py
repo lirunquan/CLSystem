73d47fe926ab89c9c5e851ac925c0250f2c9b768
lirunquan <15813388007@163.com>
2020-02-22 15:04:18 +0800

    add oj frontend

--- server/apps/oj/views.py
+++ server/apps/oj/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 from .models import Programme, Choice
 # Create your views here.
 
+
+def add_problem(request):
+    pass
+
+
+def add_programme(request):
+    pass
+
+
+def add_choice(request):
+    pass
